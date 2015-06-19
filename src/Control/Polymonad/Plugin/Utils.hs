c8492eaf9e2af824b8f632617f1251ed7520fa05
Jan Bracker <jan.bracker@googlemail.com>
2015-06-19 15:09:47 +0100

    Documentation and functions to inspect constraint arguments and type variables.

--- src/Control/Polymonad/Plugin/Utils.hs
+++ src/Control/Polymonad/Plugin/Utils.hs
@@ -1,21 +1,30 @@
- 
-module Control.Polymonad.Plugin.Utils
-  -- Plugin printing and debugging
-  ( printppr
+
+-- | Provides all kinds of functions that are needed by the plugin.
+module Control.Polymonad.Plugin.Utils ( 
+  -- * Plugin printing and debugging
+    printppr
   , printM
   , pprToStr
-  -- Polymonad module and class recognition
+  -- * Polymonad module and class recognition
   , getPolymonadModule
   , isPolymonadClass
   , getPolymonadClass
   , getPolymonadInstancesInScope
+  -- * Constraint and type inspection
   , isClassConstraint
+  , argumentTyCon, argumentTyVar
+  , collectTyVars
+  -- * General Utilities
+  , atIndex
   ) where
 
+import Data.Maybe ( listToMaybe )
+import Data.Set ( Set )
+import qualified Data.Set as S
 import Control.Monad ( filterM )
 
 import TcRnTypes
-  ( Ct
+  ( Ct(..)
   , isCDictCan_Maybe
   , isWantedCt
   , imp_mods
@@ -37,6 +46,13 @@ import InstEnv
   ( ClsInst(..)
   , instEnvElts
   , classInstances )
+import Type 
+  ( Type, TyVar
+  , getTyVar_maybe
+  , tyConAppTyCon_maybe
+  , splitTyConApp_maybe
+  )
+import TyCon ( TyCon )
 import Outputable 
   ( Outputable( ppr )
   --, text, parens, (<>)
@@ -45,13 +61,16 @@ import Outputable
 -- -----------------------------------------------------------------------------
 -- Plugin debug primitives
 -- -----------------------------------------------------------------------------
-  
+
+-- | Print some generic outputable from the plugin (Unsafe).
 printppr :: Outputable o => o -> TcPluginM ()
 printppr = tcPluginIO . putStrLn . pprToStr
 
+-- | Print a message from the plugin.
 printM :: String -> TcPluginM ()
 printM = tcPluginIO . putStrLn
 
+-- | Convert some generic outputable to a string (Unsafe).
 pprToStr :: Outputable o => o -> String
 pprToStr = showSDocUnsafe . ppr
 
@@ -116,6 +135,10 @@ getPolymonadInstancesInScope = do
       return $ classInstances instEnvs polymonadClass
     Nothing -> return []
 
+-- -----------------------------------------------------------------------------
+-- Constraint and type inspection
+-- -----------------------------------------------------------------------------
+
 -- | Check if the given constraint is a class constraint of the given class.
 isClassConstraint :: Class -> Ct -> Bool
 isClassConstraint wantedClass ct = 
@@ -123,8 +146,44 @@ isClassConstraint wantedClass ct =
     Just cls -> cls == wantedClass && isWantedCt ct
     Nothing -> False
 
+-- | @argumentTyCon n ct@ gets the type constructor of the n-th argument 
+--   in the constraint (If an n-th argument exists and actually is a type 
+--   constructor). Only works if the given constraint is a type class 
+--   constraint.
+argumentTyCon :: Int -> Ct -> Maybe TyCon
+argumentTyCon n ct = do
+  _ <- isCDictCan_Maybe ct
+  let tyArgs = cc_tyargs ct
+  tyArg <- tyArgs `atIndex` n
+  tyConAppTyCon_maybe tyArg
 
+-- | @argumentTyVar n ct@ gets the type variable of the n-th argument 
+--   in the constraint (If an n-th argument exists and actually is a type 
+--   variable). Only works if the given constraint is a type class 
+--   constraint.
+argumentTyVar :: Int -> Ct -> Maybe TyVar
+argumentTyVar n ct = do
+  _ <- isCDictCan_Maybe ct
+  let tyArgs = cc_tyargs ct
+  tyArg <- tyArgs `atIndex` n
+  getTyVar_maybe tyArg
 
+-- | Try to collect all type variables in a given expression.
+--   Only works for nested type constructor applications and type variables.
+--   If the given type is not supported an empty set is returned.
+collectTyVars :: Type -> Set TyVar
+collectTyVars t =
+  case getTyVar_maybe t of
+    Just tv -> S.singleton tv
+    Nothing -> case splitTyConApp_maybe t of
+      Just (_tc, args) -> S.unions $ fmap collectTyVars args
+      Nothing -> S.empty
 
+-- -----------------------------------------------------------------------------
+-- General utilities
+-- -----------------------------------------------------------------------------
 
+-- | Get the element of a list at a given index (If that element exists).
+atIndex :: [a] -> Int -> Maybe a
+atIndex xs i = listToMaybe $ drop i xs
 
