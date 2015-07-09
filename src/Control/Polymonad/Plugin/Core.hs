c0edb55ac5d4b7082cb783c011ed4440f45a3596
Jan Bracker <jan.bracker@googlemail.com>
2015-07-09 16:17:55 +0100

    Implement a function to pick and instance for a fully applied polymonad constraint and produce evidence

--- src/Control/Polymonad/Plugin/Core.hs
+++ src/Control/Polymonad/Plugin/Core.hs
@@ -4,33 +4,44 @@ module Control.Polymonad.Plugin.Core
   ( getPolymonadInstancesInScope
   , getPolymonadTyConsInScope
   , findMatchingInstancesForConstraint
+  , pickInstanceForAppliedConstraint
   , selectPolymonadSubset
   ) where
 
-import Data.Maybe ( isJust )
+import Data.Maybe ( isJust, isNothing, fromJust, catMaybes )
 import Data.Set ( Set )
 import qualified Data.Set as S
-import Control.Monad ( guard, MonadPlus(..) )
+import Control.Monad ( guard, forM, MonadPlus(..) )
 
-import InstEnv 
+import InstEnv
   ( ClsInst(..)
   , classInstances
-  , instanceBindFun )
+  , instanceBindFun, instanceSig
+  , lookupInstEnv
+  , lookupUniqueInstEnv )
 import TyCon ( TyCon )
-import Type ( TvSubst, TyVar, lookupTyVar )
+import Type
+  ( Type, TvSubst, TyVar
+  , lookupTyVar, getClassPredTys_maybe
+  , mkTopTvSubst, substTys )
 import Unify ( tcUnifyTys )
 import Class ( Class(..) )
-import TcRnTypes ( Ct(..) )
+import TcRnTypes ( Ct(..), CtEvidence(..) )
+import TcEvidence ( EvTerm(..) )
 import TcPluginM
 
 import Control.Polymonad.Plugin.Constraint
-  ( constraintClassType, constraintTyCons )
+  ( constraintClassType, constraintClassTyArgs
+  , constraintTyCons
+  , isClassConstraint, isFullyAppliedClassConstraint )
 import Control.Polymonad.Plugin.Instance
   ( findInstanceTopTyCons, instanceTyCons, instanceClassTyCon, instanceTyArgs, instanceTcVars )
 import Control.Polymonad.Plugin.Detect
   ( getPolymonadClass )
 import Control.Polymonad.Plugin.Utils
-  ( findConstraintOrInstanceTyCons )
+  ( findConstraintOrInstanceTyCons
+  , collectTyVars
+  , printppr, printM )
 
 -- | Returns a list of all 'Control.Polymonad' instances that are currently in scope.
 getPolymonadInstancesInScope :: TcPluginM [ClsInst]
@@ -42,7 +53,7 @@ getPolymonadInstancesInScope = do
       return $ classInstances instEnvs polymonadClass
     Nothing -> return []
 
--- | Returns the set of all type constructors in the current scope 
+-- | Returns the set of all type constructors in the current scope
 --   that are part of a polymonad in Haskell. Uses the polymonad
 --   instances to search for type constructors.
 getPolymonadTyConsInScope :: TcPluginM (Set TyCon)
@@ -65,15 +76,79 @@ findMatchingInstancesForConstraint insts ct = do
         Nothing -> mzero
     Nothing -> mzero
 
--- | Subset selection algorithm to select the correct subset of 
+-- | Given a fully applied polymonad constraint it will pick the first instance
+--   that matches it. This is ok to do, because for polymonads it does
+--   not make a difference which bind-operation we pick if the type is equal.
+pickInstanceForAppliedConstraint :: Ct -> TcPluginM (Maybe (EvTerm, Ct))
+pickInstanceForAppliedConstraint ct = do
+  -- Get the polymonad class constructor.
+  mPmCls <- getPolymonadClass
+  case (mPmCls, constraintClassTyArgs ct) of
+    -- We found the polymonad class constructor and the given constraint
+    -- is a instance constraint.
+    (Just pmCls, Just tyArgs)
+        -- Be sure to only proceed if the constraint is a polymonad constraint
+        -- and is fully applied to concrete types.
+        |  isClassConstraint pmCls ct
+        && isFullyAppliedClassConstraint ct -> do
+      -- Get the instance environment
+      instEnvs <- getInstEnvs
+      -- Find matching instance for our constraint.
+      case lookupInstEnv instEnvs pmCls tyArgs of
+        (matches, _, _) -> do
+          -- Only keep those matches that actually found a type for every argument.
+          case filter (\(_, args) -> all isJust args) matches of
+            -- If we found more then one instance, just use the first.
+            -- Because we are talking about polymonad we can freely choose.
+            (foundInst, foundInstArgs) : _ -> do
+              -- Try to produce evidence for the instance we want to use.
+              evTerm <- produceEvidenceFor foundInst (fromJust <$> foundInstArgs)
+              return $ (\ev -> (ev, ct)) <$> evTerm
+            _ -> return Nothing
+        _ -> return Nothing
+    _ -> return Nothing
+  where
+    -- | Apply the given instance dictionary to the given type arguments
+    --   and try to produce evidence for the application.
+    produceEvidenceFor :: ClsInst -> [Type] -> TcPluginM (Maybe EvTerm)
+    produceEvidenceFor inst tys = do
+      -- Get the instance type variables and constraints (by that we know the
+      -- number of type arguments and dictionart arguments for the EvDFunApp)
+      let (tyVars, cts, _cls, _tyArgs) = instanceSig inst -- ([TyVar], [Type], Class, [Type])
+      -- How the instance variables for the current instance are bound.
+      let varSubst = mkTopTvSubst $ zip tyVars tys
+      -- Global instance environment.
+      instEnvs <- getInstEnvs
+      -- Split the constraints into their class and arguments.
+      -- We ignore constraints where this is not possible.
+      -- Don't know if this is the right thing to do.
+      let instCts = catMaybes $ fmap getClassPredTys_maybe cts
+      -- Now go over each constraint and find a suitable instance and evidence.
+      ctEvTerms <- forM instCts $ \(ctCls, ctArgs) -> do
+        -- Substitute the variables to know what instance we are looking for.
+        let substArgs = substTys varSubst ctArgs
+        -- Look for suitable instance. Since we are not necessarily working
+        -- with polymonads anymore we need to find a unique one.
+        case lookupUniqueInstEnv instEnvs ctCls substArgs of
+          -- No instance found, too bad...
+          Left _err -> return Nothing
+          -- We found one: Now we can produce evidence for the found instance.
+          Right (clsInst, instArgs) -> produceEvidenceFor clsInst instArgs
+      -- If we found a good instance and evidence for every constraint,
+      -- we can create the evidence for this instance.
+      return $ if any isNothing ctEvTerms
+        then Nothing
+        else Just $ EvDFunApp (is_dfun inst) tys (fromJust <$> ctEvTerms)
+
+-- | Subset selection algorithm to select the correct subset of
 --   type constructor and bind instances that belong to the polymonad
 --   being worked with in the list of constraints.
---   
+--
 --   /Preconditions:/ For the algorithm to work correctly,
 --   certain preconditions have to be meet:
---   
+--
 --     * TODO
---   
+--
 --   __TODO: Work in Progress / Unfinished__
 selectPolymonadSubset :: [Ct] -> TcPluginM (Set TyCon, [ClsInst])
 selectPolymonadSubset cts = do
@@ -83,12 +158,12 @@ selectPolymonadSubset cts = do
     c :: Int -> TcPluginM (Set TyCon , [ClsInst])
     c 0 = do
       let initialTcs = S.unions $ fmap constraintTyCons cts
-      return (initialTcs, []) 
+      return (initialTcs, [])
     c n = do
       (initialTcs, _initialClsInsts) <- c (n - 1)
-      
+
       return (initialTcs `S.union` undefined, undefined)
-      
+
     appTC :: Set TyCon -> ClsInst -> TyVar -> TcPluginM (Set TyCon, [ClsInst])
     appTC tcsCn clsInst tcVarArg = do
       case instanceTyCons clsInst `S.isSubsetOf` tcsCn of
@@ -102,15 +177,14 @@ selectPolymonadSubset cts = do
         False -> return (S.empty, [])
 
 
--- | Substitute some type variables in the head of the given instance and 
---   look if you can find instances that provide an implementation for the 
+-- | Substitute some type variables in the head of the given instance and
+--   look if you can find instances that provide an implementation for the
 --   substituted instance. Also checks if the found instances actually
 --   support evidence.
 findApplicableInstances :: TvSubst -> ClsInst -> TcPluginM [(ClsInst, TvSubst)]
 findApplicableInstances subst clsInst = do
   let substTcVars = S.filter (isJust . lookupTyVar subst) $ instanceTcVars clsInst
   applicableTyCons <- findConstraintOrInstanceTyCons substTcVars (instanceClassTyCon clsInst, instanceTyArgs clsInst)
-  --associations 
+  --associations
   -- TODO
   return $ undefined
-
