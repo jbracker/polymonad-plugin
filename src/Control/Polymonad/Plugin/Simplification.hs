074cbdc8640dbe233aa6496cb653cd92386bd74e
Jan Bracker <jan.bracker@googlemail.com>
2015-07-09 11:48:02 +0100

    Application function for simplification rules.

--- src/Control/Polymonad/Plugin/Simplification.hs
+++ src/Control/Polymonad/Plugin/Simplification.hs
@@ -2,13 +2,19 @@
 -- | Implementation of the simplification rules for polymonad constraints
 --   described in section 5 of the "Polymonad Programming" paper (Hicks 2014).
 module Control.Polymonad.Plugin.Simplification
-  ( simplifyUp
+  ( -- * Base Simplification Rules
+    simplifyUp
   , simplifyDown
   , simplifyJoin
+    -- * Application of Simplification Rules
+  , simplifyAllUpDown
   ) where
 
-import Data.Maybe ( isJust )
+import Data.Maybe ( isJust, catMaybes )
+import Data.Set ( Set )
+import qualified Data.Set as S
 
+import Control.Applicative ( Alternative(..) )
 import Control.Monad ( guard )
 
 import Type
@@ -23,6 +29,10 @@ import Control.Polymonad.Plugin.Constraint
   ( constraintPolymonadTyArgs, constraintPolymonadTyArgs'
   , mkDerivedTypeEqCt )
 
+-- -----------------------------------------------------------------------------
+-- Base Simplification Rules
+-- -----------------------------------------------------------------------------
+
 -- | @simplifyUp idTc (psl, p, psr) rho@ tries to simplify the type variable @rho@
 --   in the constraint @p@ using the S-Up rule. The context of polymonad
 --   constraints is given by @psl@ and @psr@. The 'Identity' type constructor has to be
@@ -32,14 +42,14 @@ import Control.Polymonad.Plugin.Constraint
 --   be applied 'Nothing' is returned.
 --
 --   See figure 7 of the the "Polymonad Programming" paper for more information.
-simplifyUp :: TyCon -> ([Ct], Ct, [Ct]) -> TyVar -> Maybe Ct
+simplifyUp :: TyCon -> ([Ct], Ct, [Ct]) -> TyVar -> Maybe (TyVar, (Ct, Type))
 simplifyUp idTyCon (psl, p, psr) rho = do
   (t0, t1, t2) <- constraintPolymonadTyArgs p
   guard $ eqTyVar' rho t2 && (eqTyCon idTyCon t0 || eqTyCon idTyCon t1)
   guard $ not . null $ flowsFrom (psl ++ psr) rho
   guard $ null $ flowsTo (psl ++ psr) rho
   let m = if eqTyCon idTyCon t0 then t1 else t0
-  return $ mkDerivedTypeEqCt p rho m
+  return (rho, (p, m))
 
 -- | @simplifyDown idTc (psl, p, psr) rho@ tries to simplify the type variable @rho@
 --   in the constraint @p@ using the S-Down rule. The context of polymonad
@@ -50,14 +60,14 @@ simplifyUp idTyCon (psl, p, psr) rho = do
 --   be applied 'Nothing' is returned.
 --
 --   See figure 7 of the the "Polymonad Programming" paper for more information.
-simplifyDown :: TyCon -> ([Ct], Ct, [Ct]) -> TyVar -> Maybe Ct
+simplifyDown :: TyCon -> ([Ct], Ct, [Ct]) -> TyVar -> Maybe (TyVar, (Ct, Type))
 simplifyDown idTc (psl, p, psr) rho = do
   (t0, t1, t2) <- constraintPolymonadTyArgs p
   guard $  ( eqTyVar' rho t0 && eqTyCon idTc t1 )
         || ( eqTyVar' rho t1 && eqTyCon idTc t0 )
   guard $ null $ flowsFrom (psl ++ psr) rho
   guard $ not . null $ flowsTo (psl ++ psr) rho
-  return $ mkDerivedTypeEqCt p rho t2
+  return (rho, (p, t2))
 
 -- | @simplifyJoin ps rho@ tries to simplify the type variable @rho@
 --   in the constraints @ps@ using the S-Join rule.
@@ -73,8 +83,35 @@ simplifyJoin ps rho = do
   guard $ all isConcreteTyConApp f
   return $ mkDerivedTypeEqCt (head ps) rho (principalJoin f)
 
-isConcreteTyConApp :: Type -> Bool
-isConcreteTyConApp = isJust . tyConAppTyCon_maybe
+-- -----------------------------------------------------------------------------
+-- Simplification Application
+-- -----------------------------------------------------------------------------
+
+-- | Tries to find a simplification for the given type variable using the
+--   given set of constraints and the given simplification rule. The Identity
+--   type constructor has to be passed in.
+trySimplifyUntil :: TyCon -> [Ct] -> TyVar
+                 -> (TyCon -> ([Ct], Ct, [Ct]) -> TyVar -> Maybe (TyVar, (Ct, Type)))
+                 -> Maybe (TyVar, (Ct, Type))
+trySimplifyUntil _idTc [] _rho _simp = empty
+trySimplifyUntil idTc (ct:cts) rho simp = trySimplifyUntil' ([], ct, cts)
+  where
+    trySimplifyUntil' z@(_psl, _p, []) = simp idTc z rho
+    trySimplifyUntil' z@(psl, p, p' : psr') =
+      simp idTc z rho <|> trySimplifyUntil' (p : psl, p', psr')
+
+-- | Try to simplify as many type variables as possible in the given set using
+--   the 'simplifyUp' and 'simplifyDown' rule (in that order).
+--   The type constructor is the identity type constructor and the
+--   given constraints are the constraints to simplify.
+simplifyAllUpDown :: TyCon -> [Ct] -> Set TyVar -> [(TyVar, (Ct, Type))]
+simplifyAllUpDown idTc ps tvs = upSimps ++ downSimps
+  where
+    tvList = S.toList tvs
+    upSimps = catMaybes $ (\rho -> trySimplifyUntil idTc ps rho simplifyUp) <$> tvList
+    tvList' = S.toList $ tvs S.\\ S.fromList (fst <$> upSimps)
+    downSimps = catMaybes $ (\rho -> trySimplifyUntil idTc ps rho simplifyDown) <$> tvList'
+
 
 -- | Calculate the principal join of a set of type constructors.
 --   For this to work properly all of the given types need to be
@@ -83,6 +120,15 @@ isConcreteTyConApp = isJust . tyConAppTyCon_maybe
 --   "Polymonad Programming" paper.
 principalJoin :: [Type] -> Type
 principalJoin ts = undefined -- TODO: IMPLEMENT - How?
+
+-- -----------------------------------------------------------------------------
+-- Local Utility Functions
+-- -----------------------------------------------------------------------------
+
+-- | Check if the given type is a type constructor (not a type constructor
+--   variable) or a partial application of such.
+isConcreteTyConApp :: Type -> Bool
+isConcreteTyConApp = isJust . tyConAppTyCon_maybe
 
 -- | @flowsTo p rho@ implementats the function from Figure 7 in the paper.
 --   Returns the pairs of types that form a bind operator in @p@ together
