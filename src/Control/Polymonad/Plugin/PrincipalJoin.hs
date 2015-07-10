90f97551a99f8a31180c46174afeea975317a0bd
Jan Bracker <jan.bracker@googlemail.com>
2015-07-10 14:54:29 +0100

    Some further progress on implementing principal joins

--- src/Control/Polymonad/Plugin/PrincipalJoin.hs
+++ src/Control/Polymonad/Plugin/PrincipalJoin.hs
@@ -3,14 +3,20 @@ module Control.Polymonad.Plugin.PrincipalJoin
   ( principalJoin
   ) where
 
+import Data.Maybe ( isJust )
 import Data.Set ( Set )
 import qualified Data.Set as S
 
-import Type ( Type )
+import Control.Monad ( forM )
+
+import Type ( Type, mkTyConTy )
 import InstEnv ( ClsInst(..) )
+import TcRnTypes ( TcPluginM )
 
 import Control.Polymonad.Plugin.Utils ( collectTopTyCons )
 import Control.Polymonad.Plugin.Instance ( instanceTyCons )
+import Control.Polymonad.Plugin.Core ( pickPolymonadInstance )
+import Control.Polymonad.Plugin.Detect ( getIdentityTyCon )
 
 -- | Calculate the principal join of a set of type constructors.
 --   For this to work properly all of the given types need to be
@@ -29,7 +35,18 @@ principalJoin insts f ms = undefined -- TODO: IMPLEMENT - How?
                 `S.union` collectTopTyCons ms
                 `S.union` collectTopTyCons (concatMap (\(a,b) -> [a,b]) f)
 
-isPrincipalJoin :: [ClsInst] -> [(Type, Type)] -> [Type] -> Type -> Bool
-isPrincipalJoin insts f ms m = undefined
-  where
-    
+    isPrincipalJoin :: Type -> TcPluginM Bool
+    isPrincipalJoin m = do
+        hasOutMorphs <- and <$> forM ms (isMorphismBetween m)
+        hasInMorphs  <- and <$> forM f (`isBind` m)
+        return $ hasOutMorphs && hasInMorphs
+      where
+        isMorphismBetween :: Type -> Type -> TcPluginM Bool
+        isMorphismBetween t0 t2 = do
+          mIdTC <- getIdentityTyCon
+          case mIdTC of
+            Just idTC -> isJust <$> pickPolymonadInstance (t0, mkTyConTy idTC, t2)
+            Nothing -> return False
+
+        isBind :: (Type, Type) -> Type -> TcPluginM Bool
+        isBind (t0, t1) t2 = isJust <$> pickPolymonadInstance (t0, t1, t2)
