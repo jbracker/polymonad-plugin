6c937bb406f6e51a9c135673003d7664b3b72167
Jan Bracker <jan.bracker@googlemail.com>
2015-07-20 19:26:31 +0200

    Finished untested version of principal join algorithm.

--- src/Control/Polymonad/Plugin/PrincipalJoin.hs
+++ src/Control/Polymonad/Plugin/PrincipalJoin.hs
@@ -3,11 +3,11 @@ module Control.Polymonad.Plugin.PrincipalJoin
   ( principalJoin
   ) where
 
-import Data.Maybe ( isJust, catMaybes )
+import Data.Maybe ( isJust, catMaybes, listToMaybe )
 import Data.Set ( Set )
 import qualified Data.Set as S
 
-import Control.Monad ( forM, guard, mzero )
+import Control.Monad ( forM, guard, mzero, filterM )
 
 import Type
   ( Type, TvSubst
@@ -26,7 +26,9 @@ import Control.Polymonad.Plugin.Instance
   , instancePolymonadTyArgs )
 import Control.Polymonad.Plugin.Core ( pickPolymonadInstance )
 import Control.Polymonad.Plugin.Detect ( getIdentityTyCon )
-import Control.Polymonad.Plugin.Constraint ( constraintClassTyArgs, constraintClassType )
+import Control.Polymonad.Plugin.Constraint
+  ( constraintClassTyArgs, constraintClassType
+  , constraintPolymonadTyArgs )
 
 -- | Calculate the principal join of a set of type constructors.
 --   For this to work properly all of the given types need to be
@@ -45,7 +47,8 @@ principalJoin (insts, givenCts) f ms = if areGivenCts
     if meetsPrecond
       then do
         joinCands <- findJoinCandidates
-        undefined -- TODO: IMPLEMENT - How?
+        joins <- filterM isPrincipalJoin joinCands
+        return $ listToMaybe joins
       else return Nothing
   else return Nothing
   where
@@ -62,17 +65,16 @@ principalJoin (insts, givenCts) f ms = if areGivenCts
           isInstBind' <- isInstBind (t0, t1) t2
           return $ isInstBind' || isConstraintBind (t0, t1) t2
 
+    -- Look for candidates that could be principal joins.
     findJoinCandidates :: TcPluginM [Type]
     findJoinCandidates = do
-      -- TODO: Find candidates from instances
-
-      -- TODO: Find candidates from constraints
-
-      return undefined -- TODO
-
-    availTyCons = S.unions (instanceTyCons <$> insts)
-                `S.union` collectTopTyCons ms
-                `S.union` collectTopTyCons (concatMap (\(a,b) -> [a,b]) f)
+      -- Find candidates from instances
+      possInstCand <- concat <$> forM f (possiblePolymonadInstBindResults insts)
+      -- [(Type, [Type], ClsInst, TvSubst)]
+      -- Find candidates from constraints
+      let possCtCand = concat $ possiblePolymonadConstraintBindResults givenCts <$> f
+      -- [(Type, Ct)]
+      return $ fmap (\(x,_,_,_) -> x) possInstCand ++ fmap fst possCtCand
 
     -- Check if the given type is a principal join of f.
     isPrincipalJoin :: Type -> TcPluginM Bool
@@ -85,12 +87,13 @@ principalJoin (insts, givenCts) f ms = if areGivenCts
     isMorphismBetween t0 t2 = do
       mIdTC <- getIdentityTyCon
       case mIdTC of
-        Just idTC -> do
-          isInstMorph <- isInstBind (t0, mkTyConTy idTC) t2
-          return $ if isInstMorph
-            then True
-            else isConstraintBind (t0, mkTyConTy idTC) t2
+        Just idTC -> isBind (t0, mkTyConTy idTC) t2
         Nothing -> return False
+
+    isBind :: (Type, Type) -> Type -> TcPluginM Bool
+    isBind (t0, t1) t2 = do
+      isInstB <- isInstBind (t0, t1) t2
+      return $ isInstB || isConstraintBind (t0, t1) t2
 
     isConstraintBind :: (Type, Type) -> Type -> Bool
     isConstraintBind (t0, t1) t2 = any (eqTypes [t0, t1, t2]) givenCtArgs
@@ -117,16 +120,19 @@ possiblePolymonadInstBindResults insts (t0, t1) = fmap catMaybes $ forM insts $ 
       _ -> return Nothing
     Nothing -> return Nothing
 
--- TODO
+-- | Looks at all the given (given) constraints and if they are polymonad constraints
+--   checks the given two types match the first two arguments.
+--   Returns a list of the resulting thrid argument together with the.
 possiblePolymonadConstraintBindResults :: [Ct] -> (Type, Type)
                                        -> [(Type, Ct)]
 possiblePolymonadConstraintBindResults cts (t0, t1) = do
   ct <- cts
   guard $ isGivenCt ct
-  case constraintClassType ct of
-    Just (tyCon, tyArgs) -> undefined
+  case constraintPolymonadTyArgs ct of
+    Just (m0, m1, m2) -> if eqTypes [m0, m1] [t0, t1]
+      then return (m2, ct)
+      else mzero
     Nothing -> mzero
-  undefined
 
 
 {-
