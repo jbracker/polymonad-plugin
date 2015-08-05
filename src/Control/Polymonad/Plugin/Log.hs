0ee7bb8804a7b7c9805810473bc926da6ca2e695
Jan Bracker <jan.bracker@googlemail.com>
2015-08-05 14:27:37 +0200

    Move debugging utility functions to the Log module.

--- src/Control/Polymonad/Plugin/Log.hs
+++ src/Control/Polymonad/Plugin/Log.hs
@@ -1,8 +1,13 @@
 
+-- | Log message formatting and debuging functions.
 module Control.Polymonad.Plugin.Log
   ( pprToStr, missingCaseError
   , pmErrMsg, pmDebugMsg, pmObjMsg
+  -- * Debug Functions
+  , printTrace, printObjTrace
   ) where
+
+import Debug.Trace ( trace )
 
 import Outputable
   ( Outputable( ppr )
@@ -39,3 +44,15 @@ pmObjMsg = prefixMsg $ pluginMsgPrefix ++ "> "
 missingCaseError :: (Outputable o) => String -> Maybe o -> a
 missingCaseError funName (Just val) = error $ "Missing case in '" ++ funName ++ "' for " ++ pprToStr val
 missingCaseError funName Nothing    = error $ "Missing case in '" ++ funName ++ "'"
+
+-- -----------------------------------------------------------------------------
+-- Debug Functions
+-- -----------------------------------------------------------------------------
+
+-- | Print the result of calling 'show' on the given object.
+printTrace :: (Show a) => a -> a
+printTrace x = trace (show x) x
+
+-- | Print the result of the 'Outputable' instance of the given object.
+printObjTrace :: (Outputable o) => o -> o
+printObjTrace o = trace (pprToStr o) o
