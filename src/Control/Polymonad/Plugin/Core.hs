7252e9c519fab5d62dc7bd5918ad15eb9286b5e6
Jan Bracker <jan.bracker@googlemail.com>
2015-10-13 15:15:43 +0100

    Implement new function to support solving of ambiguous variables in indices

--- src/Control/Polymonad/Plugin/Core.hs
+++ src/Control/Polymonad/Plugin/Core.hs
@@ -2,16 +2,38 @@
 -- | A collection of functions that did not fit anywhere else.
 module Control.Polymonad.Plugin.Core
   ( isInstanceOf
+  , trySolveAmbiguousForAppliedTyConConstraint
   ) where
 
+import Data.Maybe ( fromJust, catMaybes )
+import qualified Data.Set as S
+
+import Control.Monad ( forM )
+
 import InstEnv ( ClsInst )
-import Type ( Type )
+import Type
+  ( Type, TyVar
+  , substTys, substTyVar )
+import Unify ( tcUnifyTys )
+import TcType ( isAmbiguousTyVar )
 
 import Control.Polymonad.Plugin.Environment
   ( PmPluginM, runTcPlugin
   , getGivenConstraints
-  , throwPluginError )
-import Control.Polymonad.Plugin.Instance ( isInstantiatedBy )
+  , getPolymonadClass, getCurrentPolymonad
+  , throwPluginError
+  , printObj )
+import Control.Polymonad.Plugin.Instance
+  ( isInstantiatedBy
+  , instanceTyArgs )
+import Control.Polymonad.Plugin.Constraint
+  ( WantedCt
+  , constraintClassTyArgs
+  , isClassConstraint
+  , isTyConAppliedClassConstraint )
+import Control.Polymonad.Plugin.Utils
+  ( collectTyVars
+  , skolemVarsBindFun )
 
 -- | Checks if the given arguments types to the free variables in the
 --   class instance actually form a valid instantiation of that instance.
@@ -27,3 +49,52 @@ isInstanceOf tys inst = do
   case res of
     Left err -> throwPluginError err
     Right b -> return b
+
+-- | Tries to solve ambiguous type variables in polymonad constraints using
+--   the available polymonad instances. Only unification is applied. This
+--   only delivers a result if there is exactly one instance that matches
+--   the given constraint and all type constructors of the given constraint
+--   are concrete (see 'isTyConAppliedClassConstraint').
+--
+--   __Note__: This means that ambiguous type variables in the type
+--   constructors arguments will be assigned and therefore this may not
+--   be applicable if those indices do not influence the runtime behaviour.
+trySolveAmbiguousForAppliedTyConConstraint :: WantedCt -> PmPluginM (Maybe [(TyVar, Type)])
+trySolveAmbiguousForAppliedTyConConstraint ct = do
+  pmCls <- getPolymonadClass
+  (_, pmInsts, pmCts) <- getCurrentPolymonad
+  printObj ct
+  case constraintClassTyArgs ct of
+    -- We found the polymonad class constructor and the given constraint
+    -- is a instance constraint.
+    Just tyArgs
+        -- Be sure to only proceed if the constraint is a polymonad constraint
+        -- and is fully applied to concrete types.
+        |  isClassConstraint pmCls ct
+        && isTyConAppliedClassConstraint ct -> do
+      let ambTyVars = filter isAmbiguousTyVar $ S.toList $ S.unions $ fmap collectTyVars tyArgs
+      let dontBind =  (filter (not . isAmbiguousTyVar) $ S.toList $ S.unions $ fmap collectTyVars tyArgs)
+                   ++ (S.toList $ S.unions $ concat $ fmap (maybe [] (fmap collectTyVars) . constraintClassTyArgs) pmCts)
+
+      instMatches <- forM pmInsts $ \pmInst -> do
+        printObj pmInst
+        let instArgs = instanceTyArgs pmInst
+        case tcUnifyTys (skolemVarsBindFun dontBind) tyArgs instArgs of
+          Just subst -> do
+            printObj $ substTys subst tyArgs
+            return $ Just $ zip ambTyVars (substTyVar subst <$> ambTyVars)
+          Nothing -> return Nothing
+
+      ctMatches <- forM pmCts $ \pmCt -> do
+        printObj pmCt
+        let ctArgs = fromJust $ constraintClassTyArgs pmCt
+        case tcUnifyTys (skolemVarsBindFun dontBind) tyArgs ctArgs of
+          Just subst -> do
+            printObj $ substTys subst tyArgs
+            return $ Just $ zip ambTyVars (substTyVar subst <$> ambTyVars)
+          Nothing -> return Nothing
+
+      return $ case catMaybes (instMatches ++ ctMatches) of
+        [binds] -> Just binds
+        _ -> Nothing
+    _ -> return Nothing
