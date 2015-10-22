1b062ff3121712c1703a76b6d40bb3004eebd070
Jan Bracker <jan.bracker@googlemail.com>
2015-10-22 17:57:48 +0100

    Finally, got the effect example working properly

--- examples/effect/Main.hs
+++ examples/effect/Main.hs
@@ -4,85 +4,80 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
---{-# LANGUAGE ConstraintKinds #-}
 
-module Main ( main ) where
+module Main ( main, process ) where
 
 import Prelude
 import qualified Prelude as P
-import Control.Effect
 import qualified Control.Effect as E
 import Control.Effect.State
-  ( State
-  , Var(..), Eff(..)
-  , (:->)(..), (:!)(..)
-  , get, put )
 
+ifThenElse :: Bool -> a -> a -> a
 ifThenElse True  t e = t
 ifThenElse False t e = e
 
 main :: IO ()
 main = do
   return ()
-  where --(>>=) = (P.>>=)
-        return = P.return
+  where
+    return = P.return
 
 data Tree = Leaf Int
           | Branch Tree Tree
 
-leavesV  = Var :: (Var "leaves")
-sumV     = Var :: (Var "sum")
+leavesV :: Var "leaves"
+leavesV = Var :: (Var "leaves")
+
+sumV :: Var "sum"
+sumV = Var :: (Var "sum")
+
+flattenV :: Var "flatten"
 flattenV = Var :: (Var "flatten")
 
-update :: Var n -> (a -> a) -> State '[n :-> a :! 'RW] ()
+update :: Var n -> (a -> a) -> State '[n :-> a :! 'RW] a
 update v f = do
   x <- get v
-  _ <- return 2
-  --let fx = f x
-  put v (f x) --fx
-  -- get v >>= \x -> return (f x) >>= \fx -> put v fx
-
-  -- return fx
-  where (>>=) = (E.>>=)
-        --(>>) = (E.>>)
-        return = E.return
-        fail = E.fail
-
-process :: Tree
-        -> State '[ "flatten" :-> Bool :! 'R
-                  , "leaves"  :-> Int  :! 'RW
-                  , "sum"     :-> Int  :! 'RW
-                  ] (Either Tree [Int])
-process (Leaf i) = {-do
-  update leavesV (+ 1)
-  update sumV (+ i)
-  flatten <- get flattenV
-  if flatten
-    then return $ Right [i]
-    else return $ Left $ Leaf i-}
-  update leavesV (+ 1)
-  >>
-  update sumV (+ i)
-  >>
-  get flattenV
-  >>= \flatten ->
-  if flatten
-    then return $ Right [i]
-    else return $ Left $ Leaf i
+  let fx = f x
+  put v fx
+  return fx
   where (>>=) = (E.>>=)
         (>>) = (E.>>)
         return = E.return
         fail = E.fail
 
+type ProcessEffects =
+  '[ "flatten" :-> Bool :! 'R
+   , "leaves"  :-> Int  :! 'RW
+   , "sum"     :-> Int  :! 'RW
+   ]
 {-
+-- Nubable is not exported by Control.Effect.State
+process :: (Nubable ProcessEffects)
+        => Tree
+        -> State ProcessEffects (Either Tree [Int])
+-}
+process (Leaf i) = do
+  _ <- update leavesV (+ 1)
+  _ <- update sumV (+ i)
+  flatten <- get flattenV
+  if flatten
+    then return $ Right [i]
+    else return $ Left $ Leaf i
+  where (>>=) :: (E.Inv State f g) => State f a -> (a -> State g b) -> State (E.Plus State f g) b
+        (>>=) = (E.>>=)
+        (>>) :: (E.Inv State f g) => State f a -> State g b -> State (E.Plus State f g) b
+        (>>) = (E.>>)
+        return = E.return
+        fail = E.fail
 process (Branch tl tr) = do
   eitherL <- process tl
   eitherR <- process tr
   case (eitherL, eitherR) of
     (Left  l, Left  r) -> return $ Left  $ Branch l r
     (Right l, Right r) -> return $ Right $ l ++ r
-  where (>>=) = (E.>>=)
-        --(>>) = (E.>>)
+  where (>>=) :: (E.Inv State f g) => State f a -> (a -> State g b) -> State (E.Plus State f g) b
+        (>>=) = (E.>>=)
+        (>>) :: (E.Inv State f g) => State f a -> State g b -> State (E.Plus State f g) b
+        (>>) = (E.>>)
         return = E.return
         fail = E.fail
--}
