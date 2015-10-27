1621d8850444646ad18977c93d6ca4455b649820
Jan Bracker <jan.bracker@googlemail.com>
2015-10-27 15:56:17 +0000

    Implementation of bind of effect example that is actually accepted by the compiler.

--- examples/effect/MainPolymonad.hs
+++ examples/effect/MainPolymonad.hs
@@ -24,18 +24,18 @@ import qualified Control.Effect as E
 --import Control.Effect.State ( State )
 --import qualified Control.Effect.State as ES
 
--- FIXME:
 instance (Effect m, h ~ Plus m f g, E.Inv m f g) => Polymonad (m f) (m g) (m h) where
   (>>=) = (E.>>=)
 
 instance (Effect m) => Polymonad Identity (m g) (m g) where
-  (>>=) = undefined
+  a >>= f = f (runIdentity a)
 
-instance (Effect m) => Polymonad (m f) Identity (m f) where
-  (>>=) = undefined
+instance ( Effect m, E.Inv m f (Unit m), f ~ Plus m f (Unit m))
+        => Polymonad (m f) Identity (m f) where
+  ma >>= f = ma E.>>= (E.return . runIdentity . f)
 
 instance (Effect m, h ~ Unit m) => Polymonad Identity Identity (m h) where
-  (>>=) = undefined
+  a >>= f = (E.return . runIdentity . f . runIdentity) a
 
 
 main :: IO ()
