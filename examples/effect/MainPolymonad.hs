31aeff4445bcf570fe3e3ac77d559c698af8e2b7
Jan Bracker <jan.bracker@googlemail.com>
2015-10-30 10:33:48 +0000

    Make the effect polymonad instances applicable to the given examples

--- examples/effect/MainPolymonad.hs
+++ examples/effect/MainPolymonad.hs
@@ -24,17 +24,17 @@ import qualified Control.Effect as E
 import Control.Effect.State
 --import qualified Control.Effect.State as ES
 
-instance (Effect m, h ~ Plus m f g, E.Inv m f g) => Polymonad (m f) (m g) (m h) where
+instance (Effect m, h ~ Plus m f g, E.Inv m f g) => Polymonad (m (f :: [*])) (m (g :: [*])) (m (h :: [*])) where
   (>>=) = (E.>>=)
 
-instance (Effect m) => Polymonad Identity (m g) (m g) where
+instance (Effect m) => Polymonad Identity (m (g :: [*])) (m (g :: [*])) where
   a >>= f = f (runIdentity a)
 
 instance ( Effect m, E.Inv m f (Unit m), f ~ Plus m f (Unit m))
-        => Polymonad (m f) Identity (m f) where
+        => Polymonad (m (f :: [*])) Identity (m (f :: [*])) where
   ma >>= f = ma E.>>= (E.return . runIdentity . f)
 
-instance (Effect m, h ~ Unit m) => Polymonad Identity Identity (m h) where
+instance (Effect m, h ~ Unit m) => Polymonad Identity Identity (m (h :: [*])) where
   a >>= f = (E.return . runIdentity . f . runIdentity) a
 
 main :: IO ()
@@ -59,6 +59,7 @@ update v f = do
   let fx = f x
   put v fx
   return fx
+
 {-
 type ProcessEffects =
   '[ "flatten" :-> Bool :! 'R
