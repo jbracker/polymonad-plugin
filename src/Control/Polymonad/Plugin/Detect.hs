8d667d190d69d4d4d010a81f71bfbc2423bfcd97
Jan Bracker <jan.bracker@googlemail.com>
2015-11-03 13:34:10 +0000

    Move production of evidence for instances into separate module

--- src/Control/Polymonad/Plugin/Detect.hs
+++ src/Control/Polymonad/Plugin/Detect.hs
@@ -21,11 +21,10 @@ module Control.Polymonad.Plugin.Detect
   , SubsetSelectionFunction
   , selectPolymonadByConnectedComponent
   , pickInstanceForAppliedConstraint
-  , produceEvidenceFor
   ) where
 
 import Data.Maybe
-  ( isNothing, isJust
+  ( isJust
   , catMaybes, listToMaybe
   , fromJust )
 import Data.List ( nubBy )
@@ -45,10 +44,8 @@ import TcRnTypes
 import Type
   ( Type, TyThing(..)
   , splitAppTys, eqType
-  , mkTyConTy, mkTopTvSubst
-  , substTys
-  , tyConAppTyCon_maybe
-  , getClassPredTys_maybe )
+  , mkTyConTy
+  , tyConAppTyCon_maybe )
 import TyCon ( TyCon )
 import TcPluginM
   ( TcPluginM
@@ -79,7 +76,6 @@ import InstEnv
   , instanceSig
   , classInstances
   , lookupInstEnv
-  , lookupUniqueInstEnv
   , ie_global )
 import TcEvidence ( EvTerm(..) )
 
@@ -96,6 +92,8 @@ import Control.Polymonad.Plugin.Constraint
   , isClassConstraint, isFullyAppliedClassConstraint )
 import Control.Polymonad.Plugin.Instance
   ( matchInstanceTyVars, isInstantiatedBy, eqInstance )
+import Control.Polymonad.Plugin.Evidence
+  ( produceEvidenceFor )
 
 -- -----------------------------------------------------------------------------
 -- Constant Names (Magic Numbers...)
@@ -375,41 +373,6 @@ pickInstanceForAppliedConstraint pmCls ct =
           return $ (\ev -> (ev, ct)) <$> evTerm
         _ -> return Nothing
     _ -> return Nothing
-
-
--- | Apply the given instance dictionary to the given type arguments
---   and try to produce evidence for the application.
---   The list of types has to match the number of open variables of the
---   given instance dictionary in length.
-produceEvidenceFor :: ClsInst -> [Type] -> TcPluginM (Maybe EvTerm)
-produceEvidenceFor inst tys = do
-  -- Get the instance type variables and constraints (by that we know the
-  -- number of type arguments and dictionart arguments for the EvDFunApp)
-  let (tyVars, cts, _cls, _tyArgs) = instanceSig inst -- ([TyVar], [Type], Class, [Type])
-  -- How the instance variables for the current instance are bound.
-  let varSubst = mkTopTvSubst $ zip tyVars tys
-  -- Global instance environment.
-  instEnvs <- getInstEnvs
-  -- Split the constraints into their class and arguments.
-  -- We ignore constraints where this is not possible.
-  -- Don't know if this is the right thing to do.
-  let instCts = catMaybes $ fmap getClassPredTys_maybe cts
-  -- Now go over each constraint and find a suitable instance and evidence.
-  ctEvTerms <- forM instCts $ \(ctCls, ctArgs) -> do
-    -- Substitute the variables to know what instance we are looking for.
-    let substArgs = substTys varSubst ctArgs
-    -- Look for suitable instance. Since we are not necessarily working
-    -- with polymonads anymore we need to find a unique one.
-    case lookupUniqueInstEnv instEnvs ctCls substArgs of
-      -- No instance found, too bad...
-      Left _err -> return Nothing
-      -- We found one: Now we can produce evidence for the found instance.
-      Right (clsInst, instArgs) -> produceEvidenceFor clsInst instArgs
-  -- If we found a good instance and evidence for every constraint,
-  -- we can create the evidence for this instance.
-  return $ if any isNothing ctEvTerms
-    then Nothing
-    else Just $ EvDFunApp (is_dfun inst) tys (fromJust <$> ctEvTerms)
 
 -- -----------------------------------------------------------------------------
 -- Local Utility Functions
