594dcc2a4fd5dd8737ebc3217962bc28be851268
Jan Bracker <jan.bracker@googlemail.com>
2015-11-19 11:17:20 +0000

    Thrid effect monad example now works.

--- examples/effect/MainPolymonad3.hs
+++ examples/effect/MainPolymonad3.hs
@@ -23,17 +23,19 @@ import qualified Control.Effect as E
 import Control.Effect ( Effect, Plus, Unit )
 import Control.Effect.CounterNat
 
-instance (Effect m, h ~ Plus m f g, E.Inv m f g) => Polymonad (m (f :: [*])) (m (g :: [*])) (m (h :: [*])) where
+import GHC.TypeLits ( Nat )
+
+instance (Effect m, h ~ Plus m f g, E.Inv m f g) => Polymonad (m (f :: Nat)) (m (g :: Nat)) (m (h :: Nat)) where
   (>>=) = (E.>>=)
 
-instance (Effect m) => Polymonad Identity (m (g :: [*])) (m (g :: [*])) where
+instance (Effect m) => Polymonad Identity (m (g :: Nat)) (m (g :: Nat)) where
   a >>= f = f (runIdentity a)
 
 instance ( Effect m, E.Inv m f (Unit m), f ~ Plus m f (Unit m))
-        => Polymonad (m (f :: [*])) Identity (m (f :: [*])) where
+        => Polymonad (m (f :: Nat)) Identity (m (f :: Nat)) where
   ma >>= f = ma E.>>= (E.return . runIdentity . f)
 
-instance (Effect m, h ~ Unit m) => Polymonad Identity Identity (m (h :: [*])) where
+instance (Effect m, h ~ Unit m) => Polymonad Identity Identity (m (h :: Nat)) where
   a >>= f = (E.return . runIdentity . f . runIdentity) a
 
 main :: IO ()
